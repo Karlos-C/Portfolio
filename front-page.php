8569cbd10076b2cfb191ae393a08b98615a9b507
Karlos170 <unit0shift@gmail.com>
2025-02-06 12:00:12 -0500

    Section experience

--- front-page.php
+++ front-page.php
@@ -37,4 +37,42 @@ function get_page_url_or_redirect($slug) {
     </div>
 </header>
 
+<section class="section__container skill__container" id="a-propos">
+  <div class="skill__content">
+    <h2 class="section__header">
+      À propos
+    </h2>
+    <p class="section__description">
+      Diplômé en informatique, je suis passionné par les nouvelles technologies et l’optimisation des performances. Curieux et rigoureux, j’aime relever des défis techniques et améliorer continuellement mes compétences pour proposer des solutions efficaces et adaptées.
+    </p>
+  </div>
+
+  <div class="about-col-2">
+    <div class="tab-titles">
+      <p class="tab-links active-link" onclick="opentab('skills', event)">Compétences</p>
+      <p class="tab-links" onclick="opentab('experience', event)">Expérience</p>
+      <p class="tab-links" onclick="opentab('education', event)">Éducation</p>
+    </div>
+    <div class="tab-contents active-tab" id="skills">
+      <ul>
+        <li><span>UI/UX</span><br>Designing Web/App interfaces</li>
+        <li><span>Web Development</span><br>Building Web Applications</li>
+        <li><span>Graphic Design</span><br>Creating Visual Content</li>
+      </ul>
+    </div>
+    <div class="tab-contents" id="experience">
+      <ul>
+        <li><span>Internship</span><br>Worked at XYZ Company</li>
+        <li><span>Freelancing</span><br>Worked on multiple projects</li>
+        <li><span>UI/UX</span><br>Designing Web/App interfaces</li>
+      </ul>
+    </div>
+    <div class="tab-contents" id="education">
+      <ul>
+        <li><span>2022 - 2025</span><br>DEC en Techniques de l'Informatique</li>
+      </ul>
+    </div>
+  </div>
+</section>
+
 <?php get_footer(); ?>
