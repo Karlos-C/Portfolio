d4fa97a903ba39c98ad54637bc6d86fc5a8fdc8b
Karlos170 <unit0shift@gmail.com>
2025-02-10 12:35:38 -0500

    #1 Initialisation du plugin GitHub Feed

---
+++ plugin/github-feed.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: GitHub Feed
+Description: GitHub Feed est un plugin WordPress qui permet d'afficher dynamiquement les derniers commits d’un projet GitHub directement sur les pages ou articles d'un site.
+Author: Karl-William Couturier
+Version: 1.1
+*/
+?>
