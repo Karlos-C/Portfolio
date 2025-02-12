3507ae0361bb4f961d3596e3cb148e8effc54251
Karlos170 <unit0shift@gmail.com>
2025-02-12 00:47:37 -0500

    #2 Amélioration de la page portfolio

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php get_header(); ?>
-    <main class="main">
-        <?php while(have_posts()) : the_post(); ?>
-        <article>
-            <a href=" <?php the_permalink(); ?>">
-                <?php the_post_thumbnail('medium'); ?>
-                <h2><?php the_title(); ?></h2>
-            </a>
-            <?php the_excerpt(); ?>
-        </article>
-        <?php endwhile; ?>
-    </main>
+
+    <section class="section__container">
+        <h1 class="section__header">Mes Projets</h1>
+        <div class="work-list">
+            <?php while(have_posts()) : the_post(); ?>
+                <div class="work">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('medium'); ?>
+                        <div class="layer">
+                            <h3><?php the_title(); ?></h3>
+                            <p><?php the_excerpt(); ?></p>
+                        </div>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+        </div>
+</section>
 
 <?php get_footer(); ?>
